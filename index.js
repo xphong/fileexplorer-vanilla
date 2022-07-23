ad8ab060fca4a5eb346f17e6afe14579ff1e40d5
Phong Huynh <xphong@gmail.com>
2022-07-23 13:33:27 -0400

    Add functionality to expand sidebar

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 import api from './api.js';
 
-const sidebarItemHTML = `
-  <div class="directorySidebarItemContainer">
-    <div class="directorySidebarItem">
-      <i class="fa-solid fa-caret-down"></i>
-      <div class="directorySidebarItemSelectable">
-        <i class="fa-solid fa-folder-open"></i>
-        Files
-      </div>
-    </div>
-  </div>
-`;
-
-function renderSidebarItem({ name, type, children }) {
+function renderSidebarItem({ name, type, children }, isHidden) {
   const hasChildren = !!children?.length;
-  const childrenNodes = hasChildren ? children.map((child) => renderSidebarItem(child)).join('') : '';
+  const childrenNodes = hasChildren ? children.map((child) => renderSidebarItem(child, true)).join('') : '';
 
   return type === 'folder' ? `
-  <div class="directorySidebarItemContainer">
+  <div class="directorySidebarItemContainer ${isHidden ? 'hidden' : ''}">
     <div class="directorySidebarItem">
-      ${hasChildren ? '<i class="fa-solid fa-caret-right directoryExpand"></i>' : ''}
+      ${hasChildren ? '<i class="fa-solid fa-caret-right directoryExpandToggle"></i>' : ''}
       <div class="directorySidebarItemSelectable">
         <i class="fa-solid fa-folder-open"></i>
         ${name}
@@ -30,9 +18,34 @@ function renderSidebarItem({ name, type, children }) {
   ` : '';
 }
 
+function handleSidebarExpandToggle(element) {
+  element.addEventListener('click', (event) => {
+    const containerElement = event.target.parentElement.parentElement;
+    const currentElement = event.currentTarget;
+
+    if (currentElement.classList.contains('fa-caret-right')) {
+      containerElement.querySelectorAll(':scope > .directorySidebarItemContainer').forEach((element) => {
+        element.classList.remove('hidden');
+      })
+
+      currentElement.classList.remove('fa-caret-right');
+      currentElement.classList.add('fa-caret-down');
+    } else {
+      containerElement.querySelectorAll(':scope > .directorySidebarItemContainer').forEach((element) => {
+        element.classList.add('hidden');
+      })
+
+      currentElement.classList.remove('fa-caret-down');
+      currentElement.classList.add('fa-caret-right');
+    }
+  })
+}
+
 function renderSidebar(folderDirectory) {
   const sidebar = renderSidebarItem(folderDirectory);
+
   document.querySelector('.directorySideBar').innerHTML = sidebar;
+  document.querySelectorAll('.directoryExpandToggle').forEach(handleSidebarExpandToggle);
 }
 
 async function run() {
